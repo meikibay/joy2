fde6a00bdc770855ad7fc73886b89d8a961749a1
Miguel <meikibay@gmail.com>
2014-10-10 16:33:17 +0100

    botao cima e baixo

--- main.js
+++ main.js
@@ -13,4 +13,18 @@ $(document).ready(function(){
 		newpleft = position.left -10;
 		$b.css('left', newpleft + 'px');
 	})
+
+	$('#bt3').click(function(){
+		$b = $( "#boneco" );
+		position = $b.position();
+		newpleft = position.top +10;
+		$b.css('left', newpleft + 'px');
+	})
+
+	$('#bt4').click(function(){
+		$b = $( "#boneco" );
+		position = $b.position();
+		newpleft = position.top -10;
+		$b.css('left', newpleft + 'px');
+	})
 });

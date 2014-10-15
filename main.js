8c6c2f60735dfa8619a25e38bd630a0050bb7e3a
Miguel <meikibay@gmail.com>
2014-10-15 10:20:35 +0100

    If Statements para limites

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
 		newpleft = position.left +10;
+		if (newpleft > 950) {
+			newpleft = 950
+		};
 		$b.css('left', newpleft + 'px');
 	})
 
@@ -11,6 +14,9 @@ $(document).ready(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
 		newpleft = position.left -10;
+		if (newpleft < 40) {
+			newpleft = 40
+		};
 		$b.css('left', newpleft + 'px');
 	})
 
@@ -18,6 +24,9 @@ $(document).ready(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
 		newpleft = position.top +10;
+		if (newpleft > 300) {
+			newpleft = 300
+		};
 		$b.css('top', newpleft + 'px');
 	})
 
@@ -25,6 +34,9 @@ $(document).ready(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
 		newpleft = position.top -10;
+		if (newpleft < 0) {
+			newpleft = 0
+		};
 		$b.css('top', newpleft + 'px');
 	})
 });

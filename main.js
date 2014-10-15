49056066190027a43822622ccaac43850c4ebcf0
Miguel <meikibay@gmail.com>
2014-10-15 11:55:51 +0100

    speed 30

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('#bt2').click(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
-		newpleft = position.left +10;
+		newpleft = position.left +30;
 		if (newpleft > 950) {
 			newpleft = 950
 		}
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	$('#bt1').click(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
-		newpleft = position.left -10;
+		newpleft = position.left -30;
 		if (newpleft < 40) {
 			newpleft = 40
 		}
@@ -23,7 +23,7 @@ $(document).ready(function(){
 	$('#bt3').click(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
-		newpleft = position.top +10;
+		newpleft = position.top +30;
 		if (newpleft > 300) {
 			newpleft = 300
 		}
@@ -33,7 +33,7 @@ $(document).ready(function(){
 	$('#bt4').click(function(){
 		$b = $( "#boneco" );
 		position = $b.position();
-		newpleft = position.top -10;
+		newpleft = position.top -30;
 		if (newpleft < 0) {
 			newpleft = 0
 		}

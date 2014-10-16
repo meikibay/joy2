18e72a97009890bd1d4c594e76b09e0fe4fe3c6d
Miguel <meikibay@gmail.com>
2014-10-16 11:41:27 +0100

    adicionadas todas as functions Move

--- main.js
+++ main.js
@@ -24,6 +24,28 @@ $b = $( "#boneco" );
 
 	}
 
+	function moveDown(){
+		speed=parseInt($('#txtspeed').val());
+		position = $b.position();
+		newpleft = position.top +speed;
+		if (newpleft < 150) {
+			$b.animate({"top": "+=" + speed + "px"}, "slow");
+		}
+		$( "#coordenadas" ).text(position.left + " " + position.top);
+
+	}
+
+	function moveUp(){
+		speed=parseInt($('#txtspeed').val());
+		position = $b.position();
+		newpleft = position.top -speed;
+		if (newpleft > 5) {
+			$b.animate({"top": "-=" + speed + "px"}, "slow");
+		}
+		$( "#coordenadas" ).text(position.left + " " + position.top);
+
+	}
+
 	$('#bt2').click(function(){
 		moveRight();
 	})
@@ -33,26 +55,10 @@ $b = $( "#boneco" );
 	})
 
 	$('#bt3').click(function(){
-		speed=parseInt($('#txtspeed').val());
-		position = $b.position();
-		newpleft = position.top +speed;
-		if (newpleft < 150) {
-			$b.animate({"top": "+=" + speed + "px"}, "slow");
-		}
-		//$b.css('top', newpleft + 'px');
-		//$b.animate({"top": "+=" + speed + "px"}, "slow");
-		$( "#coordenadas" ).text(position.left + " " + position.top);
+		moveDown();
 	})
 
 	$('#bt4').click(function(){
-		speed=parseInt($('#txtspeed').val());
-		position = $b.position();
-		newpleft = position.top -speed;
-		if (newpleft > 5) {
-			$b.animate({"top": "-=" + speed + "px"}, "slow");
-		}
-		//$b.css('top', newpleft + 'px');
-		//$b.animate({"top": "-=" + speed + "px"}, "slow");
-		$( "#coordenadas" ).text(position.left + " " + position.top);
+		moveUp();
 	})
 });
